6f627b00f69b8d0c82b21ed699df4d01ec3c220e
Maya abualezz <102434443+Mayaproject@users.noreply.github.com>
2023-06-14 04:16:59 +0300

    Check price and process

--- src/main_class.java
+++ src/main_class.java
@@ -1,15 +1,14 @@
+import static org.testng.Assert.assertEquals;
+
 import java.time.Duration;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Sleeper;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.google.gson.annotations.Since;
+import dev.failsafe.internal.util.Assert;
 
 public class main_class extends parameter_for_test {
 
@@ -17,6 +16,7 @@ public class main_class extends parameter_for_test {
 	public void bf() {
 		driver.get(TheWebsite);
 		driver.manage().window().maximize();
+
 	}
 
 	@Test(priority = 1)
@@ -45,21 +45,23 @@ public class main_class extends parameter_for_test {
 		SignUpButtons.click();
 
 		Thread.sleep(5000);
-		
-		WebElement ScrollDown = driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/ul/li[2]/span/button"));
+
+		WebElement ScrollDown = driver
+				.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/ul/li[2]/span/button"));
 		ScrollDown.click();
-		WebElement LogOutButtons = driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/ul/li[2]/div/ul/li[3]/a"));
+		WebElement LogOutButtons = driver
+				.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/ul/li[2]/div/ul/li[3]/a"));
 		LogOutButtons.click();
 
 //		myAssert.assertEquals(SuccessMessage, ExpectedSuccessMassege );
 
 	}
-	
 
 	@Test(priority = 2)
 	public void SignInTest() throws InterruptedException {
 		driver.get(SignInPage);
 		Thread.sleep(3000);
+
 		WebElement EmailField = driver.findElement(By.xpath("//*[@id=\"email\"]"));
 		EmailField.sendKeys(theEmailToLogin);
 
@@ -94,7 +96,7 @@ public class main_class extends parameter_for_test {
 
 				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
 				Quintyt.clear();
-				Quintyt.sendKeys("3");
+				Quintyt.sendKeys(radiantNumberToAdd);
 				Thread.sleep(1000);
 
 			} else if (driver.getCurrentUrl().contains("breathe")) {
@@ -113,7 +115,7 @@ public class main_class extends parameter_for_test {
 
 				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
 				Quintyt.clear();
-				Quintyt.sendKeys("2");
+				Quintyt.sendKeys("breatheNumberToAdd ");
 
 				WebElement AddtoCart = driver.findElement(By.xpath("//*[@id=\"product-addtocart-button\"]"));
 				AddtoCart.click();
@@ -135,7 +137,7 @@ public class main_class extends parameter_for_test {
 
 				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
 				Quintyt.clear();
-				Quintyt.sendKeys("4");
+				Quintyt.sendKeys("argusNumberToAdd ");
 
 				WebElement AddtoCart = driver.findElement(By.xpath("//*[@id=\"product-addtocart-button\"]"));
 				AddtoCart.click();
@@ -157,7 +159,7 @@ public class main_class extends parameter_for_test {
 
 				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
 				Quintyt.clear();
-				Quintyt.sendKeys("1");
+				Quintyt.sendKeys(heroNumberToAdd);
 
 				WebElement AddtoCart = driver.findElement(By.xpath("//*[@id=\"product-addtocart-button\"]"));
 				AddtoCart.click();
@@ -166,7 +168,7 @@ public class main_class extends parameter_for_test {
 			} else if (driver.getCurrentUrl().contains("backpack")) {
 				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
 				Quintyt.clear();
-				Quintyt.sendKeys("5");
+				Quintyt.sendKeys(backpackNumberToAdd);
 
 				WebElement AddtoCart = driver.findElement(By.xpath("//*[@id=\"product-addtocart-button\"]"));
 				AddtoCart.click();
@@ -175,45 +177,124 @@ public class main_class extends parameter_for_test {
 			}
 
 		}
-		
+
 	}
 
 	@Test(priority = 4)
 	public void CheckOutProcess() throws InterruptedException {
-		
+
 		// if any items still doesn't exist we make duration to let the system wait
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.get(CheckOutCart);
-		
-		WebElement Check = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/ul/li[1]/button"));
-		Check.click();
-
-	}
-	@Test(priority = 5)
-	public void ShippingDetails() throws InterruptedException {
-		
-		WebElement Company   = driver.findElement(By.xpath("//*[@id=\"IT1NX62\"]"));
-		Company.sendKeys("null");
-		WebElement StreetAddress = driver.findElement(By.xpath("//*[@id=\"RSXB3PI\"]"));
-		StreetAddress.sendKeys("Gardenz street");
-		WebElement City = driver.findElement(By.xpath("//*[@id=\"SXO7UOL\"]"));
-		City.sendKeys("Amman");
-		Select Selector = new Select(driver.findElement(By.xpath("//*[@id=\"IVSIU6J\"]")));
-		Selector.selectByIndex(RandomIndex);
-		WebElement ZipCode = driver.findElement(By.xpath("//*[@id=\"APN736Y\"]"));
-		ZipCode.sendKeys("1195");
-		WebElement PhoneNumber= driver.findElement(By.xpath("//*[@id=\"JMKMT1M\"]"));
-		PhoneNumber.sendKeys("0770706728");
-		
-		
 		Thread.sleep(2000);
+		WebElement Check = driver
+				.findElement(By.xpath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/ul/li[1]/button"));
+		Check.click();
+		Thread.sleep(5000);
+
+		WebElement StreetAddress = driver.findElement(By.name("street[0]"));
+		StreetAddress.sendKeys("Gardenz street");
+		WebElement City = driver.findElement(By.name("city"));
+		City.sendKeys("Amman");
+
+		WebElement region = driver.findElement(By.name("region_id"));
+		Select Selector = new Select(region);
+		Selector.selectByIndex(RandomIndex);
+
+		WebElement ZipCode = driver.findElement(By.name("postcode"));
+		ZipCode.sendKeys("1195");
+
+		WebElement Country = driver.findElement(By.name("country_id"));
+		Select Selector2 = new Select(Country);
+		Selector2.selectByIndex(RandomIndex);
+
+		WebElement PhoneNumber = driver.findElement(By.name("telephone"));
+		PhoneNumber.sendKeys("0770706728");
+
+		Thread.sleep(10000);
 		WebElement Next = driver.findElement(By.xpath("//*[@id=\"shipping-method-buttons-container\"]/div/button"));
 		Next.click();
+
+	}
+
+//	@Test()
+//	public void CheckPriceTest() {
+//		// -------------------
+//		// To make a List and convert it from String to Int
+//		List<String> PriceList = new ArrayList<String>();
+//		PriceList.add(radiantPrice);
+//		PriceList.add(breathePrice);
+//		PriceList.add(argusPrice);
+//		PriceList.add(heroPrice);
+//		PriceList.add(backpackPrice);
+//
+//		for (int i = 0; i < PriceList.size(); i++) {
+//
+//			String ListPriceUpdated = PriceList.get(i);
+//			String SplitPrice = ListPriceUpdated.replaceAll("\\$|\\.\\d+", "");
+//			int FinalPrice = Integer.parseInt(SplitPrice);
+//			System.out.println(FinalPrice);
+//		}
+//
+//		List<String> ItemsNumberList = new ArrayList<String>();
+//
+//		ItemsNumberList.add(radiantNumber);
+//		ItemsNumberList.add(breatheNumber);
+//		ItemsNumberList.add(argusNumber);
+//		ItemsNumberList.add(heroNumber);
+//		ItemsNumberList.add(backpackNumber);
+//
+//		for (int i = 0; i < ItemsNumberList.size(); i++) {
+//
+//			String ListNumberUpdated = ItemsNumberList.get(i);
+//			int Finalnumber = Integer.parseInt(ListNumberUpdated);
+//			System.out.println(Finalnumber);
+//
+//		}
+//
+//	}
+
+	@Test(priority = 5)
+	public void CheckThePrice() {
+
+		// price of items
+		WebElement radiantPrice = driver.findElement(By.xpath("//*[@id=\"old-price-1556-widget-product-grid\"]"));
+		WebElement breathePrice = driver.findElement(By.xpath("//*[@id=\"old-price-1812-widget-product-grid\"]"));
+		WebElement argusPrice = driver.findElement(By.xpath("//*[@id=\"old-price-694-widget-product-grid\"]"));
+		WebElement heroPrice = driver.findElement(By.xpath("//*[@id=\"old-price-158-widget-product-grid\"]"));
+		WebElement backpackPrice = driver.findElement(By.xpath("//*[@id=\"old-price-6-widget-product-grid\"]"));
+//------------------------------------
+
+		String radiantString = radiantPrice.getText().replace("$", "").replace(".", "");
+		int radiantInt = Integer.parseInt(radiantString);
+		int Finalradiantprice = radiantInt / 100;
+
+		String breatheString = breathePrice.getText().replace("$", "").replace(".", "");
+		int breatheInt = Integer.parseInt(breatheString);
+		int Finalbreatheprice = breatheInt / 100;
+
+		String argusString = argusPrice.getText().replace("$", "").replace(".", "");
+		int argusInt = Integer.parseInt(argusString);
+		int Finalargusprice = argusInt / 100;
+
+		String heroString = heroPrice.getText().replace("$", "").replace(".", "");
+		int heroInt = Integer.parseInt(heroString);
+		int Finalheroprice = heroInt / 100;
+
+		String backpackString = backpackPrice.getText().replace("$", "").replace(".", "");
+		int backpackInt = Integer.parseInt(backpackString);
+		int Finalbackpackprice = backpackInt / 100;
+
+		int ExpectedPriceOfItems = (Finalradiantprice * radiantNumber) + (Finalargusprice * argusNumber)
+				+ (Finalheroprice * heroNumber) + (Finalbreatheprice * breatheNumber)
+				+ (Finalbackpackprice * backpackNumber);
+		WebElement ActuallPrice = driver.findElement(By.xpath("//*[@id=\"opc-sidebar\"]/div[1]/table/tbody/tr[3]/td/strong/span"));
+		String StringActuall = ActuallPrice.getText().replace("$", "").replace(".", "");
+		int PriceInt = Integer.parseInt(StringActuall);
+		int Actuall = PriceInt/100 ; 
 		
+		assertEquals(Actuall, ExpectedPriceOfItems);
+		
+	
 	}
-
-	@AfterTest
-	public void at() {
-	}
-
 }
--- src/parameter_for_test.java
+++ src/parameter_for_test.java
@@ -2,48 +2,63 @@ import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.asserts.Assertion;
 
 public class parameter_for_test {
-	
-	
-	static WebDriver driver = new EdgeDriver();
-	
-	static Assertion myAssert = new Assertion();
+
+	WebDriver driver = new EdgeDriver();
+
+	Assertion myAssert = new Assertion();
 
 	Random rand = new Random();
-
+//--------------------------
+	//URLs to deal with 
+	
 	String TheWebsite = "https://magento.softwaretestingboard.com/";
 	String SignUpBage = "https://magento.softwaretestingboard.com/customer/account/create/";
 	String SignInPage = "https://magento.softwaretestingboard.com/customer/account/login/";
 	String SignOut = "href=\"https://magento.softwaretestingboard.com/customer/account/logout/";
-	
-	String [] FirstNameList = {"Khaledd","saea","Maysa","Ansaom","shukrri","saed"};
-	String [] LastNameList = {"Ahmed","Ismail","Moh","mosab","saeeed","motasem"};
-	int RandomIndex = rand.nextInt(0,5);
-	int RandomIndexEmail = rand.nextInt(50,199);
-	
-	String EmailUser = "user" ;
+	String CheckOutCart = "https://magento.softwaretestingboard.com/checkout/cart/";
+//-------------------------
+	//Information for registration 
+	String[] FirstNameList = { "Khaledd", "saea", "Maysa", "Ansaom", "shukrri", "saed" };
+	String[] LastNameList = { "Ahmed", "Ismail", "Moh", "mosab", "saeeed", "motasem" };
+	//Random
+	int RandomIndex = rand.nextInt(0, 5);
+	int RandomIndexEmail = rand.nextInt(50, 199);
+//-------------
+	//Information to Login 
+	String EmailUser = "user";
 	String EmailComplete = "@yahoo.com";
-	String theEmailToLogin ;
-	
+	String theEmailToLogin;
 	String PasswordIn = "Maya1234@@!!";
-	
-	// --------------------------------------------------
-	
+
+// --------------------------------------------------
+   // URLs to deal with 
 	String[] URLSForItems = { "https://magento.softwaretestingboard.com/radiant-tee.html",
 			"https://magento.softwaretestingboard.com/breathe-easy-tank.html",
 			"https://magento.softwaretestingboard.com/argus-all-weather-tank.html",
 			"https://magento.softwaretestingboard.com/hero-hoodie.html",
 			"https://magento.softwaretestingboard.com/fusion-backpack.html" };
-	
-	String CheckOutCart = "https://magento.softwaretestingboard.com/checkout/cart/";
-	
-	
-	
-	
-//	String SuccessMessage = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/div[2]/div/div/div")).getText();
-//	String ExpectedSuccessMassege = "Thank you for registering with Main Website Store.";
-}
 
+//-----------------------------------------
+	//number of items for Add items test
+	String radiantNumberToAdd = "3";
+	String  breatheNumberToAdd  = "2";
+	String  argusNumberToAdd  = "4";
+	String  heroNumberToAdd  = "1";
+	String  backpackNumberToAdd  = "5";
+
+// -----------------------------------
+	// number of items to add for check test
+			int radiantNumber = 3;
+			int breatheNumber = 2;
+			int argusNumber = 4;
+			int heroNumber = 1;
+			int backpackNumber = 5;
+			
+//-------------------------------------
+			
+}
